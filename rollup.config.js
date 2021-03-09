68b3c03bf74d09ea428e69209b0e38ca4aaefc59
fangt11 <fangt11@ke.com>
2021-03-09 14:35:17 +0800

    ci: rollup css

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,26 @@
 import babel from '@rollup/plugin-babel';
+import postcss from 'rollup-plugin-postcss';
 
 export default {
     input: 'src/Table/index.js',
     output: {
         file: 'dist/index.js',
-        format: 'cjs',
+        format: 'cjs'
     },
     plugins: [
+        postcss({
+            extensions: ['.css'],
+            use: [
+                [
+                    'less',
+                    {
+                        javascriptEnabled: true
+                    }
+                ]
+            ],
+            inject: true,
+            extract: false
+        }),
         babel({
             babelrc: false,
             presets: ['@babel/preset-react', '@babel/preset-env'],
