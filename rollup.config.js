66a00e56e75b149fe7a09bd83e79ee64add9424d
fangt11 <fangt11@ke.com>
2021-04-08 01:35:56 +0800

    ci: terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import babel from '@rollup/plugin-babel';
 import postcss from 'rollup-plugin-postcss';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
     input: 'lib/index.jsx',
@@ -8,7 +9,8 @@ export default {
         format: 'cjs'
     },
     plugins: [
-        postcss({ extract: true }),
+        postcss({ extract: true, minimize: true }),
+        terser(),
         babel({
             babelrc: false,
             presets: ['@babel/preset-env', '@babel/preset-react'],
