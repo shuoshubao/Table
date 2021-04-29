feb891d2f72ded39873bba1be5448438a2785e9d
fangt11 <fangt11@ke.com>
2021-04-29 20:47:44 +0800

    feat: 筛选 支持 TreeSelect

--- src/App.jsx
+++ src/App.jsx
@@ -47,6 +47,47 @@ const columns = [
         ]
     },
     {
+        title: 'Trre 筛选',
+        dataIndex: 'tree',
+        filterMultiple: true,
+        filters: [
+            {
+                title: 'Node1',
+                value: '0-0',
+                key: '0-0',
+                children: [
+                    {
+                        title: 'Child Node1',
+                        value: '0-0-0',
+                        key: '0-0-0'
+                    }
+                ]
+            },
+            {
+                title: 'Node2',
+                value: '0-1',
+                key: '0-1',
+                children: [
+                    {
+                        title: 'Child Node3',
+                        value: '0-1-0',
+                        key: '0-1-0'
+                    },
+                    {
+                        title: 'Child Node4',
+                        value: '0-1-1',
+                        key: '0-1-1'
+                    },
+                    {
+                        title: 'Child Node5',
+                        value: '0-1-2',
+                        key: '0-1-2'
+                    }
+                ]
+            }
+        ]
+    },
+    {
         title: '住址1',
         dataIndex: 'address1'
     },
