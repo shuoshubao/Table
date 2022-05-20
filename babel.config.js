9f253e33fdc5817d6ee9d4ca57320d8084358101
shuoshubao <759979885@qq.com>
2022-05-20 15:26:41 +0800

    ci: @babel/plugin-proposal-class-properties已被集成

--- babel.config.js
+++ babel.config.js
@@ -2,10 +2,9 @@
  * @Author: fangt11
  * @Date:   2022-01-27 10:42:16
  * @Last Modified by:   fangt11
- * @Last Modified time: 2022-01-27 10:48:41
+ * @Last Modified time: 2022-02-17 15:47:15
  */
 
 module.exports = {
     presets: ['@babel/preset-env', '@babel/preset-react'],
-    plugins: ['@babel/plugin-proposal-class-properties']
 };
