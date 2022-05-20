777a780c01d96d20e5286a18240b4e4a6d4c23d6
shuoshubao <759979885@qq.com>
2022-05-20 15:24:15 +0800

    feat: 表头设置

--- lib/HeaderSetting.jsx
+++ lib/HeaderSetting.jsx
@@ -1,52 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { cloneDeep, map, remove, sortBy, debounce } from 'lodash';
-import { Card, Tooltip, Dropdown, Button } from './antd';
+import { Card, Tooltip, Dropdown, Button } from 'antd';
 import SettingOutlined from '@ant-design/icons/SettingOutlined';
 import MenuOutlined from '@ant-design/icons/MenuOutlined';
 import CheckOutlined from '@ant-design/icons/CheckOutlined';
-import { ReactSortable } from 'react-sortablejs';
-import { setAsyncState, isSomeFalsy } from '@nbfe/tools';
-import { getComponentName, getStorageKey, getClassNames } from './util.jsx';
 import './index.scss';
 
 class Index extends Component {
-    static displayName = getComponentName('HeaderSetting');
+    static displayName = 'DynaTableHeaderSetting';
 
     static defaultProps = {
         columns: []
     };
 
     static propTypes = {
-        storageKey: PropTypes.string,
-        shape: PropTypes.oneOf(['button', 'icon']).isRequired,
-        columns: PropTypes.array,
-        value: PropTypes.array,
-        onChange: PropTypes.func.isRequired
+        type: PropTypes.oneOf(['button', 'icon']).isRequired,
+        columns: PropTypes.array
     };
 
     constructor(props) {
         super(props);
-        const columns = cloneDeep(props.columns);
         this.state = {
-            visible: false,
-            columns: columns,
-            selectList: []
+            visible: false
         };
         this.cardRef = React.createRef();
         this.triggerRef = React.createRef();
+        this.customEvents = this.getCustomEvents();
+        this.domEvents = this.getDomEvents();
+        this.renderResult = this.getRenderResult();
     }
 
     componentDidMount() {
-        const { value } = this.props;
-        console.log(111, value)
-        this.setState({
-            selectList: cloneDeep(value)
-        });
         document.addEventListener('click', e => {
-            if (isSomeFalsy(this.cardRef.current, this.triggerRef.current)) {
-                return;
-            }
             const isCard = this.cardRef.current.contains(e.target);
             const isTriggerEle = this.triggerRef.current.contains(e.target);
             if (isTriggerEle) {
@@ -58,130 +43,70 @@ class Index extends Component {
         });
     }
 
-    onSelect = async column => {
-        const { title } = column;
-        const { columns } = this.state;
-        const selectList = [...this.state.selectList];
-        if (selectList.includes(title)) {
-            remove(selectList, v => {
-                return v === title;
-            });
-        } else {
-            selectList.push(title);
-        }
-        await setAsyncState(this, { selectList });
-        this.update();
-    };
+    getCustomEvents() {
+        return {};
+    }
 
-    update = debounce(() => {
-        const { columns, selectList } = this.state;
-        const columnsTitleList = sortBy(selectList, v => {
-            return map(columns, 'title').indexOf(v);
-        });
-        const storageKey = getStorageKey(this.props.storageKey);
-        const columnsDataList = columnsTitleList
-            .map(v => {
-                const item = columns.find(v2 => {
-                    return (v2.title = v);
-                });
-                if (!item) {
-                    return null;
-                }
-                return v;
-            })
-            .filter(Boolean);
-        window.localStorage.setItem(storageKey, JSON.stringify(columnsDataList));
-        this.props.onChange(columnsTitleList);
-    }, 10);
+    getDomEvents() {
+        return {};
+    }
 
-    renderOverlay = () => {
-        const { state, onSelect } = this;
-        const { columns, selectList } = state;
-        return (
-            <React.Fragment>
-                <Card
-                    className={getClassNames('header-setting')}
-                    title="表头设置"
-                    style={{ width: 250 }}
-                    headStyle={{ textAlign: 'center' }}
-                    size="small"
-                    bordered={false}
-                >
-                    <div className={getClassNames('header-setting-items')} ref={this.cardRef}>
-                        <ReactSortable
-                            list={columns}
-                            setList={async columns => {
-                                await setAsyncState(this, { columns });
-                                this.update();
-                            }}
-                            handle={['.', getClassNames('header-setting-item-sort')].join('')}
-                            ghostClass={getClassNames('header-setting-item-sort-ghost')}
-                            filter={['.', getClassNames('header-setting-item-sort-disabled')].join('')}
-                            animation={150}
+    getRenderResult() {
+        return {
+            overlay: () => {
+                const { columns } = this.props;
+                return (
+                    <div ref={this.cardRef}>
+                        <Card
+                            className="dyna-table-header-setting"
+                            title="表头设置"
+                            style={{ width: 250 }}
+                            headStyle={{ textAlign: 'center' }}
+                            size="small"
+                            bordered={false}
                         >
                             {columns.map((v, i) => {
-                                const { title, canSort, canHide } = v;
+                                const { title } = v;
                                 return (
-                                    <div
-                                        className={getClassNames('header-setting-item', {
-                                            'header-setting-item-not-allowed': !canHide
-                                        })}
-                                        key={[i].join()}
-                                        onClick={() => {
-                                            if (canHide) {
-                                                onSelect(v);
-                                            }
-                                        }}
-                                    >
-                                        <div
-                                            className={getClassNames('header-setting-item-sort', {
-                                                'header-setting-item-sort-disabled': !canSort
-                                            })}
-                                            onClick={e => {
-                                                e.stopPropagation();
-                                            }}
-                                        >
+                                    <div className="dyna-table-header-setting-item" key={[i].join()}>
+                                        <div className="dyna-table-header-setting-item-sort">
                                             <MenuOutlined />
                                         </div>
-                                        <div className={getClassNames('header-setting-item-label')}>{title}</div>
-                                        <div
-                                            className={getClassNames('header-setting-item-check', {
-                                                'header-setting-item-check-disabled': !canHide
-                                            })}
-                                        >
-                                            {selectList.includes(title) && <CheckOutlined />}
+                                        <div className="dyna-table-header-setting-item-label">{title}</div>
+                                        <div className="dyna-table-header-setting-item-check">
+                                            <CheckOutlined />
                                         </div>
                                     </div>
                                 );
                             })}
-                        </ReactSortable>
+                        </Card>
                     </div>
-                </Card>
-            </React.Fragment>
-        );
-    };
+                );
+            }
+        };
+    }
 
     render() {
-        const { props, state, renderOverlay } = this;
-        const { shape } = props;
+        const { props, state, domEvents, customEvents, renderResult } = this;
+        const { type } = props;
         const { visible } = state;
-        const isIcon = shape === 'icon';
-        const buttonNode = (
-            <Button
-                type={isIcon ? '' : 'primary'}
-                icon={isIcon ? <SettingOutlined /> : null}
-                onClick={() => {
-                    this.setState({ visible: true });
-                }}
-            >
-                {isIcon ? null : '表头设置'}
-            </Button>
-        );
         return (
-            <Dropdown visible={visible} trigger={['click']} overlay={renderOverlay()}>
-                <Tooltip title={isIcon ? '表头设置' : null} placement="topRight" arrowPointAtCenter>
-                    <span ref={this.triggerRef}>{buttonNode}</span>
-                </Tooltip>
+            <Dropdown visible={visible} trigger={['click']} overlay={renderResult.overlay()}>
+                {type === 'button' ? (
+                    <Button
+                        ref={this.triggerRef}
+                        type="primary"
+                        onClick={() => {
+                            this.setState({ visible: true });
+                        }}
+                    >
+                        表头设置
+                    </Button>
+                ) : (
+                    <Tooltip title="表头设置" placement="topRight" arrowPointAtCenter>
+                        <SettingOutlined ref={this.triggerRef} />
+                    </Tooltip>
+                )}
             </Dropdown>
         );
     }
