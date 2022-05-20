1e1453e1c6b13c9b76dfb09dd0a3da5d59988c3f
shuoshubao <759979885@qq.com>
2022-05-20 15:25:46 +0800

    feat: 更名 @ke/form

--- bin/init.js
+++ bin/init.js
@@ -1,3 +1,3 @@
 const { copySync } = require('fs-extra');
 
-copySync('node_modules/@nbfe/form/dist/components.esm.js', 'lib/components.js');
+copySync('node_modules/@ke/form/dist/components.esm.js', 'lib/components.js');
